e01233713c4325620e819b41603e53199c6bba8b
abdullox0900 <abdullox0900@gmail.com>
2024-09-02 13:35:01 +0500

    feat: add async rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,11 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'uytanla.uz', 'https://uytanla.uz', 'api-maps.yandex.ru', 'core-renderer-tiles.maps.yandex.net', 'uytanla.uzhttps', 'nyc3.digitaloceanspaces.com'],
     },
-    async headers() {
+    async rewrites() {
         return [
             {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "upgrade-insecure-requests"
-                    },
-                ],
+                source: '/api/:path*',
+                destination: 'http://83.222.8.77/api/:path*',
             },
         ]
     },
